12a243e6ee2cddd85939ff005691e1a2bad555d7
Miralva <49819304+AdmxnistratorMiralva@users.noreply.github.com>
2023-11-04 20:07:55 +0300

    Add files via upload

---
+++ asdasdasdas.lua
@@ -0,0 +1,146 @@
+local ohInstance1 = workspace.HousingUnits.House["1"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+
+local ohInstance1 = workspace.HousingUnits.House["2"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["3"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["4"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["5"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["6"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["7"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["8"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["9"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["10"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["11"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["12"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["13"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["14"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["15"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["16"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["17"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["18"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["19"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["20"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["21"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["22"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["23"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["25"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["26"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["27"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["28"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+
+local ohInstance1 = workspace.HousingUnits.House["29"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["30"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["31"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["32"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["33"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["34"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["35"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["36"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["37"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["38"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+
+local ohInstance1 = workspace.HousingUnits.House["39"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["40"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["41"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["42"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["43"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["44"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["45"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["46"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["47"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["48"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["49"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
+ 
+local ohInstance1 = workspace.HousingUnits.House["50"]
+game:GetService("ReplicatedStorage").Systems.Housing.BuyPlot:InvokeServer(ohInstance1)
